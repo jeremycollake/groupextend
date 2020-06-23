aeb171dc025db1e255fadc9f8b2ad1a986af7264
Jeremy Collake <jeremy@bitsum.com>
2020-06-23 11:39:59 -0400

    remove 32-bit distribution

--- upload.cmd
+++ upload.cmd
@@ -1,6 +1,3 @@
 call bitsum-sign x64\release\groupextend.exe
-call bitsum-sign release\groupextend.exe
 pkzipc -add dist\groupextend.zip x64\release\groupextend.exe
-pkzipc -add dist\groupextend32.zip release\groupextend.exe
 scp dist\groupextend.zip jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/files/groupextend.zip
-scp dist\groupextend32.zip jeremy@az.bitsum.com:/var/www/vhosts/bitsum.com/files/groupextend32.zip
